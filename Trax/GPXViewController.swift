79d5b4781c3863fb0c6012194d882b948da638ef
yuso95 <younoussa.ousmane@icloud.com>
2017-04-03 12:17:54 -0400

    Initialization, make a new file and copy from Duliodenis (GitHub)

---
+++ Trax/GPXViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GPXViewController.swift
+//  Trax
+//
+//  Created by Younoussa Ousmane Abdou on 4/3/17.
+//  Copyright © 2017 Younoussa Ousmane Abdou. All rights reserved.
+//
+
+import UIKit
+
+class GPXViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
